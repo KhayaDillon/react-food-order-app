a95904ba3c92f90045f560972901d3a044ce4d2e
Khaya R. Dillon <yahyal0v3@yahoo.com>
2021-10-09 14:48:40 -0400

    Create Meal components and Card and Input UI components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
 import styles from './Styles.module.css'
 
 const DUMMY_MEALS = [
@@ -29,11 +31,23 @@ const DUMMY_MEALS = [
 
 
 const AvailableMeals = () => {
+    const mealList = DUMMY_MEALS.map(meal => 
+        <MealItem 
+            key={meal.id} 
+            id={meal.id} 
+            name={meal.name} 
+            description={meal.description} 
+            price={meal.price} 
+        />
+    );
+    
     return (
         <section className={styles.meals}>
-            <ul>
-                {DUMMY_MEALS.map(meal => <li>{meal.name}</li>)}
-            </ul>
+            <Card>
+                <ul>
+                    {mealList}
+                </ul>
+            </Card>
         </section>
     )
 };
